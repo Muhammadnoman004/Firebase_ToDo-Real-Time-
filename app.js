34cd8dc6a3d167341ac6e054cb58a09c7efda2b2
Muhammad Noman <143292643+Muhammadnoman004@users.noreply.github.com>
2023-12-06 18:52:14 +0500

    Almost done

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getDatabase, ref, set, push, onValue , remove , update} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  push,
+  onValue,
+  remove,
+  update,
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDlzNer2kChEaFoLw2X4sbUTbG3JqWFFbk",
@@ -7,112 +15,110 @@ const firebaseConfig = {
   projectId: "todo-app-real-time",
   storageBucket: "todo-app-real-time.appspot.com",
   messagingSenderId: "773115970194",
-  appId: "1:773115970194:web:c6ebb2d8192dedc4755c4b"
+  appId: "1:773115970194:web:c6ebb2d8192dedc4755c4b",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
 let getinp = document.querySelector("#inp");
-let getaddbtn = document.querySelector("#add")
+let getaddbtn = document.querySelector("#add");
 let getul = document.querySelector("#ul");
 let getclearbtn = document.querySelector("#clear");
 
+//  Add ToDo in Database  //
 
 getaddbtn.addEventListener("click", async () => {
-
   if (getinp.value == "") {
-    alert("please Enter a value")
-  }
-  else {
-    getul.innerHTML = " "
-    const dataRef = push(ref(db, `users/Karachi`))
+    alert("please Enter a value");
+  } else {
+    const dataRef = push(ref(db, `users/Karachi`));
     set(dataRef, {
-      ToDo: getinp.value
+      ToDo: getinp.value,
     });
   }
+});
 
-})
+//  Read ToDo from Database //
 
 function getData() {
-
   const starCountRef = ref(db, `users/Karachi`);
   onValue(starCountRef, (snapshot) => {
     const data = snapshot.val();
-    // console.log(data);
+    getul.innerHTML = " ";
 
     for (const key in data) {
-
       const showToDo = data[key].ToDo;
-      //  console.log(key);
 
       let getli = document.createElement("li");
-      getli.className = "list"
+      getli.className = "list";
       let liTextDiv = document.createElement("div");
-      liTextDiv.setAttribute("class", "liTextDiv")
+      liTextDiv.setAttribute("class", "liTextDiv");
       let liTextPara = document.createElement("p");
       let litext = document.createTextNode(showToDo);
 
       liTextPara.appendChild(litext);
       liTextDiv.appendChild(liTextPara);
-      getli.appendChild(liTextDiv)
+      getli.appendChild(liTextDiv);
       getul.appendChild(getli);
-      getinp.value = ""
+      getinp.value = "";
 
       let div = document.createElement("div");
       div.classList.add("divClass");
 
       let editIcon = document.createElement("i");
-      editIcon.setAttribute('onclick', `editTodo(this)`)
-      editIcon.className = 'edit'
+      editIcon.setAttribute("onclick", `editTodo(this, '${key}')`);
+      editIcon.className = "edit";
       editIcon.classList.add("fa");
       editIcon.classList.add("fa-pencil-square");
 
-
       let delIcon = document.createElement("i");
-      delIcon.setAttribute("onclick", `delTodo('${key}')`)
-      delIcon.className = "deleted"
+      delIcon.setAttribute("onclick", `delTodo('${key}')`);
+      delIcon.className = "deleted";
       delIcon.classList.add("fa");
       delIcon.classList.add("fa-trash");
-      div.appendChild(editIcon)
-      div.appendChild(delIcon)
+      div.appendChild(editIcon);
+      div.appendChild(delIcon);
       getli.appendChild(div);
-
-
     }
-
   });
 }
-getData()
+getData();
 
+//  Delete ToDo From Database //
 
 function delTodo(id) {
-  getul.innerHTML = " "
   console.log(id);
-  const deltodo = ref(db , `users/Karachi/${id}`)
-  remove(deltodo)
+  const deltodo = ref(db, `users/Karachi/${id}`);
+  remove(deltodo);
 }
 
-function editTodo(e , id) {
-  let editToDo = prompt("Edit your ToDo", e.parentNode.parentNode.firstChild.textContent)
-  e.parentNode.parentNode.firstChild.textContent = editToDo
-  // console.log(editToDo);
-  // const ToDoedit = ref(db , `users/Karachi/${id}`)
+//  Edit ToDo in Database //
 
+function editTodo(e, id) {
+  let editToDo = prompt(
+    "Edit your ToDo",
+    e.parentNode.parentNode.firstChild.textContent
+  );
 
-  // console.log(ToDoedit);
-  // const EditToDo = {
-  //   ToDo: editToDo
-  // }
-  // update(ToDoedit , EditToDo )
+  if (editToDo) {
+    const ToDoedit = ref(db, `users/Karachi/${id}`);
+
+    const EditToDo = {
+      ToDo: editToDo,
+    };
+
+    update(ToDoedit, EditToDo);
+  }
 }
 
+//  Delete All from Database  //
 
 getclearbtn.addEventListener("click", () => {
-  getul.innerHTML = ""
-  const clearTodo = ref(db ,  `users/Karachi/`)
-  remove(clearTodo)
-})
+  getul.innerHTML = "";
+  const clearTodo = ref(db, `users/Karachi/`);
+  remove(clearTodo);
+});
 
-window.delTodo = delTodo
-window.editTodo = editTodo
+window.delTodo = delTodo;
+window.editTodo = editTodo;
